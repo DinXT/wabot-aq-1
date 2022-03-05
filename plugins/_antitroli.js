592336c01c2bd818e2d999db8b1d39a4ac8c7224
Pencuri eSCeh :v <92778687+DinXT@users.noreply.github.com>
2022-03-05 15:25:17 +0700

    bug troli ngeselin jdi gw singkat aj :v

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -1,7 +1,7 @@
 module.exports = {
     async all(m) {
         if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
-            m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
+            m.reply('Anjay fake troli :v')
             // await this.clearMessage(m.chat, m.key)
             await this.modifyChat(m.chat, 'clear', {
                 includeStarred: false
